ec2ae3c8a5eb0d69b6002c0d33494f984cae4e8b
DevinAnnunzio <annunzio.da@gmail.com>
2023-10-27 19:12:42 +0900

    Feat: Form with validation finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'forms-td-assignment';
 
   @ViewChild('form') myForm
+
+  subscription: string
+  subscriptionOptions = ['Basic', 'Advanced', 'Pro']
+
+  // ngOnInIt(){
+  //   this.subscription = 'Advanced'
+  // }
+
+  constructor(){
+    this.subscription = 'Advanced'
+  }
 
   onSubmit(){
     console.log(this.myForm)
